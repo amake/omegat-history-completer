90461cb00cdbafbdd6237e459ccc8df319c7613f
Aaron Madlon-Kay <aaron@madlon-kay.com>
2016-03-09 21:17:23 +0900

    Update copyright

--- src/main/java/org/madlonkay/history/HistoryCompleter.java
+++ src/main/java/org/madlonkay/history/HistoryCompleter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Aaron Madlon-Kay <aaron@madlon-kay.com>
+ * Copyright (C) 2016 Aaron Madlon-Kay <aaron@madlon-kay.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
